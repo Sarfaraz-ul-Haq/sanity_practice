b7639644987032c6822d9683de15dea73b451667
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-14 11:55:27 +0500

    practice

---
+++ sanity/schema/pet.ts
@@ -0,0 +1,29 @@
+const pet = {
+  title: "Planet",
+  name: "planet",
+  type: "document",
+  fields: [
+    {
+      title: "Planet Name",
+      name: "planetName",
+      type: "string",
+    },
+    {
+      title: "Diameter",
+      name: "diameter",
+      type: "number",
+    },
+    {
+      title: "Mass",
+      name: "mass",
+      type: "string",
+    },
+    {
+      title: "Image",
+      name: "image",
+      type: "image",
+    },
+  ],
+};
+
+export default pet;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
 import Image from "next/image";
+import { client } from "../lib/sanityClient";
+import { urlForImage } from "../../sanity/lib/image";
 
-export default function Home() {
-  return <div>Learning to fetch data from sanity</div>;
+const getPlanetsData = async () => {
+  const res = await client.fetch(
+    `*[_type == "planet"]{planetName, image, diameter, mass}`
+  );
+  return res;
+};
+
+async function Home() {
+  const data = await getPlanetsData();
+  console.log(data);
+
+  return (
+    <main className="flex gap-5 justify-center">
+      {data.map((planet: any) => (
+        <div key={planet.id} className="border-md border-2">
+          <div>{planet.planetName}</div>
+          <Image
+            src={urlForImage(planet.image)}
+            alt={`${planet.planetName} Photo`}
+            width={100}
+            height={100}
+          />
+          <div>Diameter: {planet.diameter}</div>
+          <div>Mass: {planet.mass}</div>
+        </div>
+      ))}
+    </main>
+  );
 }
+
+export default Home;
